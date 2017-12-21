d0b084865ddb0540c6b5fbb264bf5f8a06792957
oketels <olivier.ketels@gmail.com>
2017-12-21 12:42:48 +0100

    Use thymeleaf.

--- src/main/java/be/xplore/cicddemo/HelloWorldController.java
+++ src/main/java/be/xplore/cicddemo/HelloWorldController.java
@@ -1,18 +1,21 @@
 package be.xplore.cicddemo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author <a mailto="olivier.ketels@xplore.be">Olivier Ketels</a>
  */
 
-@RestController
+@Controller
 public class HelloWorldController {
 
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot !!! (updated for testing purposes)";
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
 }
