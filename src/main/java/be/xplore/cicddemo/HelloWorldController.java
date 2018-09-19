605aac4a2e334540d1cf8d9a19ed769b1e746012
Olivier Ketels <olivier.ketels@gmail.com>
2018-09-19 13:38:58 +0200

    Small change to trigger pipeline.

--- src/main/java/be/xplore/cicddemo/HelloWorldController.java
+++ src/main/java/be/xplore/cicddemo/HelloWorldController.java
@@ -5,6 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * @author <a mailto="olivier.ketels@xplore.be">Olivier Ketels</a>
  */
@@ -16,6 +20,15 @@ public class HelloWorldController {
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+
+
+    @RequestMapping(value = "/action")
+    public Map<String, Object> doSomeThing(){
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
     }
 
 }
